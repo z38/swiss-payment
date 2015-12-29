7ea5f40759527ca04c3838f7ac50a50781ffdb8d
z38 <z38@z38.invalid>
2015-12-29 11:15:18 +0100

    Test operands of mixed currencies

---
+++ tests/Z38/SwissPayment/Tests/Money/MixedTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Z38\SwissPayment\Tests\Money;
+
+use Z38\SwissPayment\Money;
+use Z38\SwissPayment\Tests\TestCase;
+
+class MixedTest extends TestCase
+{
+    /**
+     * @covers \Z38\SwissPayment\Money\Mixed::add
+     */
+    public function testAdd()
+    {
+        $sum = new Money\Mixed(0);
+        $sum = $sum->plus(new Money\CHF(4300));
+        $sum = $sum->plus(new Money\EUR(1200));
+
+        $this->assertEquals('55.00', $sum->format());
+    }
+
+    /**
+     * @covers \Z38\SwissPayment\Money\Mixed::minus
+     */
+    public function testMinus()
+    {
+        $sum = new Money\Mixed(10000);
+        $sum = $sum->minus(new Money\CHF(5000));
+        $sum = $sum->minus(new Money\EUR(300));
+
+        $this->assertEquals('47.00', $sum->format());
+    }
+}
