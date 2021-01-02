68da9d5ed1efcc052f99f3fafcd4efed0dd6d6ea
wlcoo <8859093+wlcoo@users.noreply.github.com>
2021-01-02 01:59:14 +0100

    Validation Bugfix
    Validates for https://xml-validator.nordea.lt/pain_validation/validate

--- src/Z38/SwissPayment/Message/CustomerCreditTransfer.php
+++ src/Z38/SwissPayment/Message/CustomerCreditTransfer.php
@@ -90,7 +90,7 @@ class CustomerCreditTransfer extends AbstractMessage
      */
     protected function getSchemaName()
     {
-        return 'http://www.six-interbank-clearing.com/de/pain.001.001.03.ch.02.xsd';
+        return 'urn:iso:std:iso:20022:tech:xsd:pain.001.001.03';
     }
 
     /**
